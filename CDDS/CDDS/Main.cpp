60dc8921c415018ebdcd8415e31bfcc7029c3ab3
Joshua Albe <joshjalbe@gmail.com>
2021-01-27 15:31:24 -0600

    Bubble sort exercise

---
+++ CDDS/CDDS/Main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+//Create a function that will sort an array of integers using bubble sort
+//Print the array before and after it's sorted
+
+void bubbleSort(int arr[], int length)
+{
+	for (int i = 0; i < length; i++) 
+	{
+		for (int j = length - 1; j > i; j--) 
+		{
+			if (arr[j] < arr[j - 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j - 1];
+				arr[j - 1] = temp;
+			}
+		}
+	}
+}
+
+void print(int array[], int length) 
+{
+	//Iterate through the array
+	for (int i = 0; i < length; i++) 
+	{
+		//Sending the indexed number to the console
+		std::cout << array[i];
+
+		if (i < length - 1) 
+		{
+			//Comma for clarity
+			std::cout << ", ";
+		}
+	}
+	//End the line
+	std::cout << std::endl;
+	system("pause");
+}
+
+int main() 
+{
+	//Create a new array
+	int exampleArr[5] = { 4,2,5,1,3 };
+
+	//Print array
+	print(exampleArr, 5);
+
+	//Sort array
+	bubbleSort(exampleArr, 5);
+
+	//Print sorted array
+	print(exampleArr, 5);
+}
